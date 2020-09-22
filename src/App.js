f03cff209a7236355dd55434186e98beb3d99f59
sonjaolson <sonjaolson@newrelic.com>
2020-09-22 16:54:52 -0700

    add react-spring

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
 import { AiFillLinkedin } from "react-icons/ai";
 import "./App.css";
+import { useSpring, animated } from "react-spring";
 
 const Bio = styled.div`
   margin: 0 auto;
 `;
 function App() {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="portfolio-img" alt="logo" />
         <Bio>
-          Sonja Olson <br />
+          <animated.div style={props}>Sonja Olson</animated.div>
           <FaGithub href="https://github.com/olsonso" />
           <AiFillLinkedin href="https://www.linkedin.com/in/olsonso/" />
         </Bio>
